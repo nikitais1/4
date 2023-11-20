70d090d5b985080ecb0b27096d78b4e2532b977f
Nikita <nikita.isaev2001@mail.ru>
2023-11-20 22:46:13 +0300

    add HEADERS_SJ

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
-URL_HH = "https://api.hh.ru/vacancies"
+URL_HH = 'https://api.hh.ru/vacancies'
+URL_SJ = 'https://api.superjob.ru/2.0/vacancies/'
+HEADERS_SJ = 'v3.r.137945941.84cf2d532b12bd08bc320ce6ba2255cd6619e46d.3bb52fb483728decee19b2fa04e5c37d231d47fd'
+
 JSON_HH = Path(Path(__file__).parent, 'src/cache_json', 'cache_hh.json')
 JSON_SJ = Path(Path(__file__).parent, 'src/cache_json', 'cache_sj.json')
 
-URL_SJ = 'https://api.superjob.ru/2.0/vacancies/'
