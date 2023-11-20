54b3b9c4575aec0cda8b21e2268f94a7b3a23099
Nikita <nikita.isaev2001@mail.ru>
2023-11-20 22:45:54 +0300

    add func user_interaction()

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from src.data_json.work_with_json import WorkWithJsonHH, WorkWithJsonSJ
-from src.request.api import HH, SJ
-from src.vacancy.vacancy import VacancySJ, VacancyHH
+from src.dialogue import user_interaction
 
 if __name__ == '__main__':
-    search_query = input("Введите поисковый запрос: ").title().strip()
-    if isinstance(search_query, str) and search_query.isalpha():
-        hh = HH(search_query)
-        sj = SJ(search_query)
-        hh.get_vacancies()
-        sj.get_vacancies()
-
-        wwj_hh = WorkWithJsonHH()
-        wwj_sj = WorkWithJsonSJ()
-        vacancy_hh = wwj_hh.read_json()
-        vacancy_sj = wwj_sj.read_json()
-        # список со всеми вакансиями по ключевому слову
-        vacancy_s = []
-        for i in vacancy_hh:
-            vacancy_s.append(
-                VacancyHH(i['name'], i['apply_alternate_url'], i['snippet']['requirement'], i['snippet']['responsibility'], i['salary']['from'], i['salary']['to']))
-
-        for i in vacancy_sj:
-            vacancy_s.append(VacancySJ(i['profession'], i['link'], i['candidat'], i['payment_from'], i['payment_to']))
-
-        for vacancy in vacancy_s:
-            print(vacancy)
+    user_interaction()
