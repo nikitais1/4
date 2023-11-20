2ecce33e307b71d845e27268d617b1fb7c503b6d
Nikita <nikita.isaev2001@mail.ru>
2023-11-20 16:03:47 +0300

    changed api.py

--- src/request/api.py
+++ src/request/api.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import requests
 from config import URL_HH, URL_SJ
+from src.data_json.work_with_json import WorkWithJsonHH, WorkWithJsonSJ
 
 
 class API(ABC):
@@ -23,7 +24,7 @@ class HH(API):
 
     def get_vacancies(self):
         response = requests.get(url=self.url, params=self.params)
-        return response.json()
+        return WorkWithJsonHH.save_json(response.json()['items'])
 
 
 class SJ(API):
@@ -39,4 +40,4 @@ class SJ(API):
             'X-Api-App-Id': 'v3.r.137945941.84cf2d532b12bd08bc320ce6ba2255cd6619e46d.3bb52fb483728decee19b2fa04e5c37d231d47fd'
         }
         response = requests.get(url=self.url, params=self.params, headers=headers)
-        return response.json()
+        return WorkWithJsonSJ.save_json(response.json()['objects'])
